4aa3d583f2f9396efb6411086e6b4aae1a953df9
liyanan <liyanan@yeah.net>
2024-07-28 21:11:50 +0800

    更新CartpoleDemo

---
+++ ocs2_robotic_examples/doc/doc.md
@@ -0,0 +1,9 @@
+
+
+
+## ocs2_mobile_manipulator
+franka/task.info里把panda的手指自由度panda_finger_joint1和panda_finger_joint2给去掉了。
+关于此部分，可参考[【OCS2机器人强化学习】初学教程（1）](https://www.bilibili.com/video/BV1kZ421p71A/?spm_id_from=333.337.search-card.all.click&vd_source=6a0c03d9a0385c892327b2ad71dccfb7) ———— 截图panda.png
+
+
+# 学习资料
--- ocs2_robotic_examples/ocs2_cartpole/test/CartpoleDemo.cpp
+++ ocs2_robotic_examples/ocs2_cartpole/test/CartpoleDemo.cpp
@@ -19,234 +19,87 @@ enum class PenaltyType {
   ModifiedRelaxedBarrierPenalty,
 };
 
-class Cartpole {
-public:
-  static constexpr ocs2::scalar_t timeHorizon = 15.0;
-  static constexpr ocs2::scalar_t goalViolationTolerance = 1e-1;
-  static constexpr ocs2::scalar_t constraintViolationTolerance = 1e-1;
-
-  Cartpole(ocs2::ddp::Algorithm algo, PenaltyType penalty) {
-    algorithm = algo;
-    penaltyType = penalty;
-    // interface
-    taskFile = ocs2::cartpole::getPath() + "/config/mpc/task.info";
-    const std::string libFolder = ocs2::cartpole::getPath() + "/auto_generated";
-    cartPoleInterfacePtr.reset(new ocs2::cartpole::CartPoleInterface(taskFile, libFolder, true /*verbose*/));
-
-    // Since the problem only uses final cost for swing-up, the final cost should be scaled proportional to
-    // the increase of time horizon. We fist remove the final cost and then add a scaled version
-    const std::string finalCostName = "finalCost";
-    if (!cartPoleInterfacePtr->optimalControlProblem().finalCostPtr->erase(finalCostName)) {
-      throw std::runtime_error("[Cartpole::Cartpole]: " + finalCostName + " was not found!");
-    }
-    auto createFinalCost = [&]() {
-      ocs2::matrix_t Qf(ocs2::cartpole::STATE_DIM, ocs2::cartpole::STATE_DIM);
-      ocs2::loadData::loadEigenMatrix(taskFile, "Q_final", Qf);
-      Qf *= (timeHorizon / cartPoleInterfacePtr->mpcSettings().timeHorizon_);  // scale cost
-      return std::make_unique<ocs2::QuadraticStateCost>(Qf);
-    };
-    cartPoleInterfacePtr->optimalControlProblem().finalCostPtr->add(finalCostName, createFinalCost());
-
-    // remove InputLimits as it will be added later in each test
-    inputLimitsConstraint = popInequalityLagrangian(inputLimitsName, cartPoleInterfacePtr->optimalControlProblem());
-
-    // initial command
-    initTargetTrajectories.timeTrajectory.push_back(0.0);
-    initTargetTrajectories.stateTrajectory.push_back(cartPoleInterfacePtr->getInitialTarget());
-    initTargetTrajectories.inputTrajectory.push_back(ocs2::vector_t::Zero(ocs2::cartpole::INPUT_DIM));
-  }
-
-  std::unique_ptr<ocs2::GaussNewtonDDP> getAlgorithm() const {
-    auto ddpSettings = cartPoleInterfacePtr->ddpSettings();
-    ddpSettings.algorithm_ = algorithm;
-    ddpSettings.maxNumIterations_ = 100;
-    ddpSettings.minRelCost_ = 1e-12;  // to avoid early termination
-    ddpSettings.displayInfo_ = false;
-    ddpSettings.displayShortSummary_ = true;
-
-    switch (algorithm) {
-      case ocs2::ddp::Algorithm::SLQ:
-        return std::make_unique<ocs2::SLQ>(std::move(ddpSettings), cartPoleInterfacePtr->getRollout(),
-                                     createOptimalControlProblem(),
-                                     cartPoleInterfacePtr->getInitializer());
-      case ocs2::ddp::Algorithm::ILQR:
-        return std::make_unique<ocs2::ILQR>(std::move(ddpSettings), cartPoleInterfacePtr->getRollout(),
-                                      createOptimalControlProblem(),
-                                      cartPoleInterfacePtr->getInitializer());
-      default:
-        throw std::runtime_error("[Cartpole::getAlgorithm] undefined algorithm");
-    }
-  }
-
-  std::unique_ptr<ocs2::augmented::AugmentedPenaltyBase> getPenalty() const {
-    switch (penaltyType) {
-      case PenaltyType::SlacknessSquaredHingePenalty: {
-        using penalty_type = ocs2::augmented::SlacknessSquaredHingePenalty;
-        penalty_type::Config boundsConfig;
-        ocs2::loadData::loadPenaltyConfig(taskFile, "bounds_penalty_config", boundsConfig, false);
-        return penalty_type::create(boundsConfig);
-      }
-      case PenaltyType::ModifiedRelaxedBarrierPenalty: {
-        using penalty_type = ocs2::augmented::ModifiedRelaxedBarrierPenalty;
-        penalty_type::Config boundsConfig;
-        ocs2::loadData::loadPenaltyConfig(taskFile, "bounds_penalty_config", boundsConfig, false);
-        return penalty_type::create(boundsConfig);
-      }
-      default:
-        throw std::runtime_error("[Cartpole::getPenalty] undefined penaltyType");
-    }
-  }
-
-  ocs2::OptimalControlProblem createOptimalControlProblem() const {
-    ocs2::OptimalControlProblem problem = cartPoleInterfacePtr->getOptimalControlProblem();
-    problem.inequalityLagrangianPtr->add(inputLimitsName, ocs2::create(std::unique_ptr<ocs2::StateInputConstraint>(inputLimitsConstraint->clone()), getPenalty()));
-    return problem;
-  }
-
-  std::unique_ptr<ocs2::StateInputConstraint> popInequalityLagrangian(const std::string& name, ocs2::OptimalControlProblem& ocp) const {
-    auto termLagrangianPtr = ocp.inequalityLagrangianPtr->extract(name);
-    if (termLagrangianPtr == nullptr) {
-      throw std::runtime_error("[Cartpole::popInequalityLagrangian]: " + name + " was not found!");
-    }
-
-    auto termStateInpuLagrangianPtr = dynamic_cast<ocs2::StateInputAugmentedLagrangian*>(termLagrangianPtr.get());
-    if (termStateInpuLagrangianPtr == nullptr) {
-      throw std::runtime_error("[Cartpole::popInequalityLagrangian]: term " + name + " is not of type StateInputAugmentedLagrangian!");
-    }
-
-    return std::unique_ptr<ocs2::StateInputConstraint>(termStateInpuLagrangianPtr->get().clone());
-  }
-
-  const std::string inputLimitsName = "InputLimits";
-  ocs2::TargetTrajectories initTargetTrajectories;
-  std::unique_ptr<ocs2::cartpole::CartPoleInterface> cartPoleInterfacePtr;
-
-  std::string taskFile;
-  std::unique_ptr<ocs2::StateInputConstraint> inputLimitsConstraint;
-
-  ocs2::ddp::Algorithm algorithm; 
-  PenaltyType penaltyType;
-};
-
-int CartpoleDDP(ocs2::ddp::Algorithm algo, PenaltyType penalty) {
-  Cartpole cart_pole(algo, penalty);
-  // construct solver
-  auto ddpPtr = cart_pole.getAlgorithm();
-
-  // set TargetTrajectories
-  ddpPtr->getReferenceManager().setTargetTrajectories(cart_pole.initTargetTrajectories);
-
-  // observer for InputLimits violation
-  auto inputLimitsObserverModulePtr = ocs2::SolverObserver::LagrangianTermObserver(
-      ocs2::SolverObserver::Type::Intermediate, 
-      "InputLimits", 
-      [&](const ocs2::scalar_array_t& timeTrajectory, const std::vector<ocs2::LagrangianMetricsConstRef>& termMetrics) {
-        for (size_t i = 0; i < timeTrajectory.size(); i++) {
-          const ocs2::vector_t constraintViolation = termMetrics[i].constraint.cwiseMin(0.0);
-          if (std::fabs(constraintViolation(0)) > cart_pole.constraintViolationTolerance) {
-            LOG(ERROR) << "Input lower limit is violated at time " << std::to_string(timeTrajectory[i]);
-          }
-          if (std::fabs(constraintViolation(1)) > cart_pole.constraintViolationTolerance) {
-            LOG(ERROR) << "Input upper limit is violated at time " << std::to_string(timeTrajectory[i]);
-          }
-        }
-      }
-    );
-  ddpPtr->addSolverObserver(std::move(inputLimitsObserverModulePtr));
-
-  // run solver
-  ddpPtr->run(0.0, cart_pole.cartPoleInterfacePtr->getInitialState(), cart_pole.timeHorizon);
-
-  // test final state
-  const auto& finalState = ddpPtr->primalSolution(cart_pole.timeHorizon).stateTrajectory_.back();
-  const auto& desiredState = cart_pole.cartPoleInterfacePtr->getInitialTarget();
-  LOG(INFO) << "Pole final angle, final_state=" << finalState(0) << ", desired_state=" << desiredState(0);
-  LOG(INFO) << "Cart final position, final_state=" << finalState(1) << ", desired_state=" << desiredState(1);
-  LOG(INFO) << "Pole final velocity, final_state=" << finalState(2) << ", desired_state=" << desiredState(2);
-  LOG(INFO) << "Cart final velocity, final_state=" << finalState(3) << ", desired_state=" << desiredState(3);
-}
-
-std::unique_ptr<ocs2::StateInputConstraint> PopInequalityLagrangian(const std::string& name, ocs2::OptimalControlProblem& ocp) {
-  auto termLagrangianPtr = ocp.inequalityLagrangianPtr->extract(name);
-  if (termLagrangianPtr == nullptr) {
-    throw std::runtime_error("[Cartpole::popInequalityLagrangian]: " + name + " was not found!");
-  }
-  auto termStateInpuLagrangianPtr = dynamic_cast<ocs2::StateInputAugmentedLagrangian*>(termLagrangianPtr.get());
-  if (termStateInpuLagrangianPtr == nullptr) {
-    throw std::runtime_error("[Cartpole::popInequalityLagrangian]: term " + name + " is not of type StateInputAugmentedLagrangian!");
-  }
-  return std::unique_ptr<ocs2::StateInputConstraint>(termStateInpuLagrangianPtr->get().clone());
-}
-
-std::unique_ptr<ocs2::augmented::AugmentedPenaltyBase> getPenalty(std::string task_file, PenaltyType penalty) {
-  switch (penalty) {
-    case PenaltyType::SlacknessSquaredHingePenalty: {
-      using penalty_type = ocs2::augmented::SlacknessSquaredHingePenalty;
-      penalty_type::Config boundsConfig;
-      ocs2::loadData::loadPenaltyConfig(task_file, "bounds_penalty_config", boundsConfig, false);
-      return penalty_type::create(boundsConfig);
-    }
-    case PenaltyType::ModifiedRelaxedBarrierPenalty: {
-      using penalty_type = ocs2::augmented::ModifiedRelaxedBarrierPenalty;
-      penalty_type::Config boundsConfig;
-      ocs2::loadData::loadPenaltyConfig(task_file, "bounds_penalty_config", boundsConfig, false);
-      return penalty_type::create(boundsConfig);
-    }
-    default:
-      throw std::runtime_error("[Cartpole::getPenalty] undefined penaltyType");
-  }
-}
-
-std::unique_ptr<ocs2::GaussNewtonDDP> getAlgorithm(std::string task_file, ocs2::ddp::Algorithm algorithm, PenaltyType penalty, ocs2::cartpole::CartPoleInterface& cart_pole_interface) {
-  cart_pole_interface.ddpSettings().algorithm_ = algorithm;
-  cart_pole_interface.ddpSettings().maxNumIterations_ = 100;
-  cart_pole_interface.ddpSettings().minRelCost_ = 1e-12;  // to avoid early termination
-  cart_pole_interface.ddpSettings().displayInfo_ = false;
-  cart_pole_interface.ddpSettings().displayShortSummary_ = true;
-  // remove InputLimits as it will be added later in each test
-  const std::string input_limits_name = "InputLimits";
-  std::unique_ptr<ocs2::StateInputConstraint> input_limits_constraint = PopInequalityLagrangian(input_limits_name, cart_pole_interface.optimalControlProblem());
-  ocs2::OptimalControlProblem problem = cart_pole_interface.getOptimalControlProblem();
-  problem.inequalityLagrangianPtr->add(input_limits_name, ocs2::create(std::unique_ptr<ocs2::StateInputConstraint>(input_limits_constraint->clone()), getPenalty(task_file, penalty)));
-  switch (algorithm) {
-    case ocs2::ddp::Algorithm::SLQ:
-      return std::make_unique<ocs2::SLQ>(std::move(cart_pole_interface.ddpSettings()), cart_pole_interface.getRollout(),
-                                   problem,
-                                   cart_pole_interface.getInitializer());
-    case ocs2::ddp::Algorithm::ILQR:
-      return std::make_unique<ocs2::ILQR>(std::move(cart_pole_interface.ddpSettings()), cart_pole_interface.getRollout(),
-                                    problem,
-                                    cart_pole_interface.getInitializer());
-    default:
-      throw std::runtime_error("[Cartpole::getAlgorithm] undefined algorithm");
-  }
-}
-
 int TestCartpoleDDP(ocs2::ddp::Algorithm algo, PenaltyType penalty) {
   std::string task_file = ocs2::cartpole::getPath() + "/config/mpc/task.info";
   const std::string lib_folder = ocs2::cartpole::getPath() + "/auto_generated";
   ocs2::cartpole::CartPoleInterface cart_pole_interface(task_file, lib_folder, true);
   // Since the problem only uses final cost for swing-up, the final cost should be scaled proportional to the increase of time horizon. We fist remove the final cost and then add a scaled version
-  const std::string final_cost_name = "finalCost";
-  if (!cart_pole_interface.optimalControlProblem().finalCostPtr->erase(final_cost_name)) {
-    throw std::runtime_error("[Cartpole::Cartpole]: " + final_cost_name + " was not found!");
-  }
   const ocs2::scalar_t time_horizon = 15.0;
-  ocs2::matrix_t Qf(ocs2::cartpole::STATE_DIM, ocs2::cartpole::STATE_DIM);
-  ocs2::loadData::loadEigenMatrix(task_file, "Q_final", Qf);
-  Qf *= (time_horizon / cart_pole_interface.mpcSettings().timeHorizon_);  // scale cost
-  cart_pole_interface.optimalControlProblem().finalCostPtr->add(final_cost_name, std::make_unique<ocs2::QuadraticStateCost>(Qf));
+  {
+    const std::string final_cost_name = "finalCost";
+    if (!cart_pole_interface.optimalControlProblem().finalCostPtr->erase(final_cost_name)) {
+      throw std::runtime_error("[Cartpole::Cartpole]: " + final_cost_name + " was not found!");
+    }
+    ocs2::matrix_t Qf(ocs2::cartpole::STATE_DIM, ocs2::cartpole::STATE_DIM);
+    ocs2::loadData::loadEigenMatrix(task_file, "Q_final", Qf);
+    Qf *= (time_horizon / cart_pole_interface.mpcSettings().timeHorizon_);  // scale cost
+    cart_pole_interface.optimalControlProblem().finalCostPtr->add(final_cost_name, std::make_unique<ocs2::QuadraticStateCost>(Qf));
+  }
   // initial command
   ocs2::TargetTrajectories init_target_trajectories;
   init_target_trajectories.timeTrajectory.push_back(0.0);
   init_target_trajectories.stateTrajectory.push_back(cart_pole_interface.getInitialTarget());
   init_target_trajectories.inputTrajectory.push_back(ocs2::vector_t::Zero(ocs2::cartpole::INPUT_DIM));
   // construct solver
-  std::unique_ptr<ocs2::GaussNewtonDDP> ddp_ptr = getAlgorithm(task_file, algo, penalty, cart_pole_interface);
-
+  std::unique_ptr<ocs2::GaussNewtonDDP> ddp_ptr;
+  {
+    cart_pole_interface.ddpSettings().algorithm_ = algo;
+    cart_pole_interface.ddpSettings().maxNumIterations_ = 100;
+    cart_pole_interface.ddpSettings().minRelCost_ = 1e-12;  // to avoid early termination
+    cart_pole_interface.ddpSettings().displayInfo_ = false;
+    cart_pole_interface.ddpSettings().displayShortSummary_ = true;
+    const std::string input_limits_name = "InputLimits";
+    // remove InputLimits as it will be added later
+    std::unique_ptr<ocs2::StateInputConstraint> input_limits_constraint;
+    {
+      auto termLagrangianPtr = cart_pole_interface.optimalControlProblem().inequalityLagrangianPtr->extract(input_limits_name);
+      if (termLagrangianPtr == nullptr) {
+        throw std::runtime_error("[Cartpole::popInequalityLagrangian]: " + input_limits_name + " was not found!");
+      }
+      auto termStateInpuLagrangianPtr = dynamic_cast<ocs2::StateInputAugmentedLagrangian*>(termLagrangianPtr.get());
+      if (termStateInpuLagrangianPtr == nullptr) {
+        throw std::runtime_error("[Cartpole::popInequalityLagrangian]: term " + input_limits_name + " is not of type StateInputAugmentedLagrangian!");
+      }
+      input_limits_constraint = std::unique_ptr<ocs2::StateInputConstraint>(termStateInpuLagrangianPtr->get().clone());
+    }
+    // getPenalty
+    std::unique_ptr<ocs2::augmented::AugmentedPenaltyBase> augmented_penalty;
+    {
+      switch (penalty) {
+        case PenaltyType::SlacknessSquaredHingePenalty: {
+          using penalty_type = ocs2::augmented::SlacknessSquaredHingePenalty;
+          penalty_type::Config boundsConfig;
+          ocs2::loadData::loadPenaltyConfig(task_file, "bounds_penalty_config", boundsConfig, false);
+          augmented_penalty = penalty_type::create(boundsConfig);
+          break;
+        }
+        case PenaltyType::ModifiedRelaxedBarrierPenalty: {
+          using penalty_type = ocs2::augmented::ModifiedRelaxedBarrierPenalty;
+          penalty_type::Config boundsConfig;
+          ocs2::loadData::loadPenaltyConfig(task_file, "bounds_penalty_config", boundsConfig, false);
+          augmented_penalty = penalty_type::create(boundsConfig);
+          break;
+        }
+        default:
+          throw std::runtime_error("[Cartpole::getPenalty] undefined penaltyType");
+      }
+    }
+    // add InputLimits by penalty
+    cart_pole_interface.getOptimalControlProblem().inequalityLagrangianPtr->add(input_limits_name, ocs2::create(std::unique_ptr<ocs2::StateInputConstraint>(input_limits_constraint->clone()), std::move(augmented_penalty)));
+    // getAlgorithm
+    switch (algo) {
+      case ocs2::ddp::Algorithm::SLQ:
+        ddp_ptr = std::make_unique<ocs2::SLQ>(std::move(cart_pole_interface.ddpSettings()), cart_pole_interface.getRollout(), cart_pole_interface.getOptimalControlProblem(), cart_pole_interface.getInitializer());
+        break;
+      case ocs2::ddp::Algorithm::ILQR:
+        ddp_ptr = std::make_unique<ocs2::ILQR>(std::move(cart_pole_interface.ddpSettings()), cart_pole_interface.getRollout(), cart_pole_interface.getOptimalControlProblem(), cart_pole_interface.getInitializer());
+        break;
+      default:
+        throw std::runtime_error("[Cartpole::getAlgorithm] undefined algorithm");
+    }
+  }
   // set TargetTrajectories
   ddp_ptr->getReferenceManager().setTargetTrajectories(init_target_trajectories);
-
   // observer for InputLimits violation
   const ocs2::scalar_t constraint_violation_tolerance = 1e-1;
   auto inputLimitsObserverModulePtr = ocs2::SolverObserver::LagrangianTermObserver(
@@ -281,7 +134,6 @@ int TestCartpoleDDP(ocs2::ddp::Algorithm algo, PenaltyType penalty) {
 
 int main() {
   LOG(INFO) << "CartpoleDemo start";
-  // CartpoleDDP(ocs2::ddp::Algorithm::SLQ, PenaltyType::SlacknessSquaredHingePenalty);
   TestCartpoleDDP(ocs2::ddp::Algorithm::SLQ, PenaltyType::SlacknessSquaredHingePenalty);
   LOG(INFO) << "CartpoleDemo end";
   return 0;
